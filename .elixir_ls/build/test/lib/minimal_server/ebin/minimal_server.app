89f7db3af22dc324f0cd22eee2475c205532e058
Bartosz Michalak <bartosz.michalak@smart-citizen.pl>
2020-02-07 14:33:55 +0100

    Init repository. Add working app with one endpoint

---
+++ .elixir_ls/build/test/lib/minimal_server/ebin/minimal_server.app
@@ -0,0 +1,9 @@
+{application,minimal_server,
+             [{applications,[kernel,stdlib,elixir,logger,poison,cowboy,plug]},
+              {description,"minimal_server"},
+              {modules,['Elixir.MinimalServer',
+                        'Elixir.MinimalServer.Application',
+                        'Elixir.MinimalServer.Endpoint']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {mod,{'Elixir.MinimalServer.Application',[]}}]}.
---
+++ lib/minimal_server/endpoint.ex
@@ -0,0 +1,30 @@
+defmodule MinimalServer.Endpoint do
+  use Plug.Router
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts) do
+    Plug.Adapters.Cowboy2.http(__MODULE__, [])
+  end
+
+  forward("/bot", to: MinimalServer.Router)
+
+  match _ do
+    send_resp(conn, 404, "Requested page not found!")
+  end
+end
